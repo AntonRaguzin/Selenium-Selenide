48b2fb0f04e2f892a6eab31a20e698733513e938
antonraguzin <raguzin82@gmail.com>
2022-08-09 19:33:46 +0300

    adden new tests

--- src/test/java/CreditCardFormTestSelenium.java
+++ src/test/java/CreditCardFormTestSelenium.java
@@ -41,4 +41,32 @@ public class CreditCardFormTestSelenium {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldFindImputErrorFieldName() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Anton");
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText().trim();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindImputErrorFieldPhone() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Антон");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("89998887766");
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText().trim();
+        Assertions.assertEquals(expected, actual);
+    }
+
+//    @Test
+//    void shouldNotBeSentWithoutCheckbox() {
+//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Антонов Антон");
+//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998887766");
+//        driver.findElement(By.className("button__text")).click();
+//        String expected = driver.findElement(By.className("checkbox__text")).getCssValue(".input_invalid");
+//        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
