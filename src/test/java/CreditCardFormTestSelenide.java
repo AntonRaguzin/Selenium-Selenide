d7d65b7ad1975e53a719d149cbf2876447d5b832
antonraguzin <raguzin82@gmail.com>
2022-08-09 16:47:56 +0300

    added webDriverManager for selenium

--- src/test/java/CreditCardFormTestSelenide.java
+++ src/test/java/CreditCardFormTestSelenide.java
@@ -2,7 +2,6 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
